0e7d6ddfa43f22b9e4ff6f2c9a69ae9a84e0ac02
MaartentePoele <maarten.te.poele@hva.nl>
2026-02-18 10:50:29 +0100

    #4 Extra filters toegevoegd voor teams

--- server.js
+++ server.js
@@ -70,22 +70,60 @@ app.get("/", async function (request, response) {
   }
 
   // Filter op team
-  if (request.query.filter == "Cheer") {
+  if (request.query.filter == "Awesome") {
+    params["filter[team][_eq]"] = "Awesome";
+  } else if (request.query.filter == "Blaze") {
+    params["filter[team][_eq]"] = "Blaze";
+  } else if (request.query.filter == "Bliss") {
+    params["filter[team][_eq]"] = "Bliss";
+  } else if (request.query.filter == "Cheer") {
     params["filter[team][_eq]"] = "Cheer";
+  } else if (request.query.filter == "Chill") {
+    params["filter[team][_eq]"] = "Chill";
+  } else if (request.query.filter == "Cool") {
+    params["filter[team][_eq]"] = "Cool";
   } else if (request.query.filter == "Dazzle") {
     params["filter[team][_eq]"] = "Dazzle";
+  } else if (request.query.filter == "Delight") {
+    params["filter[team][_eq]"] = "Delight";
+  } else if (request.query.filter == "Epic") {
+    params["filter[team][_eq]"] = "Epic";
+  } else if (request.query.filter == "Flex") {
+    params["filter[team][_eq]"] = "Flex";
+  } else if (request.query.filter == "Flux") {
+    params["filter[team][_eq]"] = "Flux";
+  } else if (request.query.filter == "Fun") {
+    params["filter[team][_eq]"] = "Fun";
   } else if (request.query.filter == "Glow") {
     params["filter[team][_eq]"] = "Glow";
+  } else if (request.query.filter == "Happy") {
+    params["filter[team][_eq]"] = "Happy";
   } else if (request.query.filter == "Harmony") {
     params["filter[team][_eq]"] = "Harmony";
+  } else if (request.query.filter == "Hype") {
+    params["filter[team][_eq]"] = "Hype";
+  } else if (request.query.filter == "Jolly") {
+    params["filter[team][_eq]"] = "Jolly";
+  } else if (request.query.filter == "Joy") {
+    params["filter[team][_eq]"] = "Joy";
+  } else if (request.query.filter == "Peak") {
+    params["filter[team][_eq]"] = "Peak";
+  } else if (request.query.filter == "Rad") {
+    params["filter[team][_eq]"] = "Rad";
   } else if (request.query.filter == "Radiant") {
     params["filter[team][_eq]"] = "Radiant";
   } else if (request.query.filter == "Rocket") {
     params["filter[team][_eq]"] = "Rocket";
   } else if (request.query.filter == "Spark") {
     params["filter[team][_eq]"] = "Spark";
+  } else if (request.query.filter == "Spirit") {
+    params["filter[team][_eq]"] = "Spirit";
+  } else if (request.query.filter == "Storm") {
+    params["filter[team][_eq]"] = "Storm";
   } else if (request.query.filter == "Sunny") {
     params["filter[team][_eq]"] = "Sunny";
+  } else if (request.query.filter == "Zen") {
+    params["filter[team][_eq]"] = "Zen";
   } else {
     params["sort"] = "name";
   }
@@ -134,37 +172,6 @@ app.get("/", async function (request, response) {
     persons: personResponseJSON.data,
   });
 });
-
-app.post("/", async function (request, response) {
-  // Stuur een POST request naar de messages tabel
-  // Een POST request bevat ook extra parameters, naast een URL
-  await fetch("https://fdnd.directus.app/items/messages", {
-    // Overschrijf de standaard GET method, want ook hier gaan we iets veranderen op de server
-    method: "POST",
-
-    // Geef de body mee als JSON string
-    body: JSON.stringify({
-      // Dit is zodat we ons bericht straks weer terug kunnen vinden met ons filter
-      for: `Team ${teamName}`,
-      // En dit zijn onze formuliervelden
-      from: request.body.from,
-      text: request.body.text,
-    }),
-
-    // En vergeet deze HTTP headers niet: hiermee vertellen we de server dat we JSON doorsturen
-    // (In realistischere projecten zou je hier ook authentication headers of een sleutel meegeven)
-    headers: {
-      "Content-Type": "application/json;charset=UTF-8",
-    },
-  });
-
-  // Stuur de browser daarna weer naar de homepage
-  response.redirect(303, "/");
-});
-
-// ---------------
-// TESTEN MET POST
-// ---------------
 
 app.get("/:id", async function (request, response) {
   const personDetailResponse = await fetch(
@@ -225,9 +232,49 @@ app.post("/:id/unlike", async function (request, response) {
   response.redirect(303, "/");
 });
 
-// ------------
-// EINDE TESTEN
-// ------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -247,7 +294,7 @@ app.post("/:id/unlike", async function (request, response) {
 
 
 // ------------------------------
-// CODE VOOR METE VANAF REGEL 250
+// CODE VOOR METE VANAF REGEL 300
 // ------------------------------
 
 app.get("/", async function (request, response) {
